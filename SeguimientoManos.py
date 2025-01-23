fff6189b23384a0fa7846a250a7fc06ca78c6263
Jair <jairalejandromerchan@gmail.com>
2025-01-23 16:22:48 -0500

    "Updated Imagenes/0.png"

--- SeguimientoManos.py
+++ SeguimientoManos.py
@@ -1,28 +1,27 @@
-
-#------------------------------Importamos las librerias -----------------------------------
+#------------------------------ Importamos las librerías -----------------------------------
 import math
 import cv2
 import mediapipe as mp
 import time
 
-#-------------------------------- Creamos una clase---------------------------------
+#-------------------------------- Creamos una clase ---------------------------------
 class detectormanos():
-    #-------------------Inicializamos los parametros de la deteccion----------------
-    def __init__(self, mode=False, maxManos = 2, model_complexity=1, Confdeteccion = 0.5, Confsegui = 0.5):
-        self.mode = mode          #Creamos el objeto y el tendra su propia variable
-        self.maxManos = maxManos  #Lo mismo haremos con todos los objetos
+    #------------------- Inicializamos los parámetros de la detección ----------------
+    def __init__(self, mode=False, maxManos=2, model_complexity=1, Confdeteccion=0.5, Confsegui=0.5):
+        self.mode = mode          # Creamos el objeto y el tendrá su propia variable
+        self.maxManos = maxManos  # Lo mismo haremos con todos los objetos
         self.compl = model_complexity
         self.Confdeteccion = Confdeteccion
         self.Confsegui = Confsegui
 
-        # ---------------------------- Creamos los objetos que detectaran las manos y las dibujaran----------------------
+        # ---------------------------- Creamos los objetos que detectarán las manos y las dibujarán ----------------------
         self.mpmanos = mp.solutions.hands
         self.manos = self.mpmanos.Hands(self.mode, self.maxManos, self.compl, self.Confdeteccion, self.Confsegui)
         self.dibujo = mp.solutions.drawing_utils
-        self.tip = [4,8,12,16,20]
+        self.tip = [4, 8, 12, 16, 20]
 
-    #----------------------------------------Funcion para encontrar las manos-----------------------------------
-    def encontrarmanos(self, frame, dibujar = True ):
+    #---------------------------------------- Función para encontrar las manos -----------------------------------
+    def encontrarmanos(self, frame, dibujar=True):
         imgcolor = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         self.resultados = self.manos.process(imgcolor)
 
@@ -32,8 +31,8 @@ class detectormanos():
                     self.dibujo.draw_landmarks(frame, mano, self.mpmanos.HAND_CONNECTIONS)  # Dibujamos las conexiones de los puntos
         return frame
 
-    #------------------------------------Funcion para encontrar la posicion----------------------------------
-    def encontrarposicion(self, frame, ManoNum = 0, dibujar = True, color = []):
+    #------------------------------------ Función para encontrar la posición ----------------------------------
+    def encontrarposicion(self, frame, ManoNum=0, dibujar=True, color=[]):
         xlista = []
         ylista = []
         bbox = []
@@ -43,75 +42,80 @@ class detectormanos():
             miMano = self.resultados.multi_hand_landmarks[ManoNum]
             prueba = self.resultados.multi_hand_landmarks
             player = len(prueba)
-            #print(player)
             for id, lm in enumerate(miMano.landmark):
                 alto, ancho, c = frame.shape  # Extraemos las dimensiones de los fps
-                cx, cy = int(lm.x * ancho), int(lm.y * alto)  # Convertimos la informacion en pixeles
+                cx, cy = int(lm.x * ancho), int(lm.y * alto)  # Convertimos la información en píxeles
                 xlista.append(cx)
                 ylista.append(cy)
                 self.lista.append([id, cx, cy])
                 if dibujar:
-                    cv2.circle(frame,(cx, cy), 3, (0, 0, 0), cv2.FILLED)  # Dibujamos un circulo
+                    cv2.circle(frame, (cx, cy), 3, (0, 0, 0), cv2.FILLED)  # Dibujamos un círculo
 
             xmin, xmax = min(xlista), max(xlista)
             ymin, ymax = min(ylista), max(ylista)
             bbox = xmin, ymin, xmax, ymax
             if dibujar:
                 # Dibujamos cuadro
-                cv2.rectangle(frame,(xmin - 20, ymin - 20), (xmax + 20, ymax + 20), color,2)
+                cv2.rectangle(frame, (xmin - 20, ymin - 20), (xmax + 20, ymax + 20), color, 2)
         return self.lista, bbox, player
 
-    #----------------------------------Funcion para detectar y dibujar los dedos arriba------------------------
+    #---------------------------------- Función para detectar y dibujar los dedos arriba ------------------------
     def dedosarriba(self):
         dedos = []
-        if self.lista[self.tip[0]][1] > self.lista[self.tip[0]-1][1]:
+        if self.lista[self.tip[0]][1] > self.lista[self.tip[0] - 1][1]:
             dedos.append(1)
         else:
             dedos.append(0)
 
-        for id in range (1,5):
-            if self.lista[self.tip[id]][2] < self.lista[self.tip[id]-2][2]:
+        for id in range(1, 5):
+            if self.lista[self.tip[id]][2] < self.lista[self.tip[id] - 2][2]:
                 dedos.append(1)
             else:
                 dedos.append(0)
 
         return dedos
 
-    #--------------------------- Funcion para detectar la distancia entre dedos----------------------------
-    def distancia(self, p1, p2, frame, dibujar = True, r = 15, t = 3):
+    #--------------------------- Función para detectar la distancia entre dedos ----------------------------
+    def distancia(self, p1, p2, frame, dibujar=True, r=15, t=3):
         x1, y1 = self.lista[p1][1:]
         x2, y2 = self.lista[p2][1:]
         cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
         if dibujar:
-            cv2.line(frame, (x1,y1), (x2,y2), (0,0,255),t)
-            cv2.circle(frame, (x1,y1), r, (0,0,255), cv2.FILLED)
-            cv2.circle(frame, (x2,y2), r, (0, 0, 255), cv2.FILLED)
-            cv2.circle(frame, (cx,cy), r, (0, 0, 255), cv2.FILLED)
-        length = math.hypot(x2-x1, y2-y1)
+            cv2.line(frame, (x1, y1), (x2, y2), (0, 0, 255), t)
+            cv2.circle(frame, (x1, y1), r, (0, 0, 255), cv2.FILLED)
+            cv2.circle(frame, (x2, y2), r, (0, 0, 255), cv2.FILLED)
+            cv2.circle(frame, (cx, cy), r, (0, 0, 255), cv2.FILLED)
+        length = math.hypot(x2 - x1, y2 - y1)
 
         return length, frame, [x1, y1, x2, y2, cx, cy]
 
-#----------------------------------------------- Funcion principal-------------------- ----------------------------
+#----------------------------------------------- Función principal -------------------------------------------------
 def main():
     ptiempo = 0
     ctiempo = 0
 
-    # -------------------------------------Leemos la camara web ---------------------------------------------
+    # ------------------------------------- Leemos la cámara web ---------------------------------------------
     cap = cv2.VideoCapture(0)
-    #-------------------------------------Crearemos el objeto -------------------------------------
+
+    # Configuramos los FPS de la cámara, si es posible
+    cap.set(cv2.CAP_PROP_FPS, 60)  # Cambia 60 por el valor máximo soportado por tu cámara
+
+    # ------------------------------------- Creamos el objeto -------------------------------------
     detector = detectormanos()
-    # ----------------------------- Realizamos la deteccion de manos---------------------------------------
+
+    # ----------------------------- Realizamos la detección de manos ---------------------------------------
     while True:
         ret, frame = cap.read()
-        #Una vez que obtengamos la imagen la enviaremos
+        
+        if not ret:
+            print("No se pudo leer el frame de la cámara.")
+            break
+
+        # Una vez que obtengamos la imagen, la enviaremos
         frame = detector.encontrarmanos(frame)
         lista, bbox, _ = detector.encontrarposicion(frame)
 
-
-
-        #if len(lista) != 0:
-            #print(lista[4])
-        # ----------------------------------------Mostramos los fps ---------------------------------------
+        # ---------------------------------------- Mostramos los FPS ---------------------------------------
         ctiempo = time.time()
         fps = 1 / (ctiempo - ptiempo)
         ptiempo = ctiempo
@@ -121,13 +125,11 @@ def main():
         cv2.imshow("Manos", frame)
         k = cv2.waitKey(1)
 
-        if k == 27:
+        if k == 27:  # Presionar "Esc" para salir
             break
+
     cap.release()
     cv2.destroyAllWindows()
-
-
-
 
 if __name__ == "__main__":
     main()
